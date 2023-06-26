79e14480accf13dcb941d01c5b6b08217c8306fd
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-26 17:07:47 +0200

    fixx style in about.blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.base')
 
 @section('contents')
-    <h2>About Us</h2>
+    <h2 class="text-center">About Us</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente error veniam minus sit voluptas modi odit veritatis, mollitia non dolores doloremque asperiores ducimus vitae assumenda est vel id cupiditate.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente error veniam minus sit voluptas modi odit veritatis, mollitia non dolores doloremque asperiores ducimus vitae assumenda est vel id cupiditate.</p>
 @endsection
