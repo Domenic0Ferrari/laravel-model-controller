3176af175d44ff282775995e934d35927a563d61
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-26 16:34:36 +0200

    add complete card in movies.blade.php

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -6,12 +6,12 @@
         <div class="row">
             @foreach ($colMovies as $movie)
             <div class="col-4">
-                <div class="card">
+                <div class="card h-100">
                     <h5>{{ $movie->title }}</h5>
-                    <h6></h6>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <h6>{{ $movie->original_title }}</h6>
+                    <div>{{ $movie->nationality }}</div>
+                    <div>{{ $movie->date }}</div>
+                    <div>{{ $movie->vote }}</div>
                 </div>
             </div>
             @endforeach
