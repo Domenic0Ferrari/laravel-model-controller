f512d1c0e77c8d29087868931388ac83eace249c
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-26 16:08:48 +0200

    add partials and menu.php

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,13 @@
+@php
+    $menuItems = config('menu')
+@endphp
+
+<nav>
+    <ul>
+        @foreach ($menuItems as $menuItem)
+            <li>
+                <a href="{{ route($menuItem['route']) }}"> {{ $menuItem['name'] }} </a>
+            </li>
+        @endforeach
+    </ul>
+</nav>
